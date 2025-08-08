fab8949f935c4c13cd6f0f601d8870e39733b8a0
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-09 00:46:48 +0600

    5th commit

---
+++ slice.js
@@ -0,0 +1,13 @@
+const country = "Bangladesh";
+const part = country.slice(2,5);
+console.log(part)
+
+const name ="My name is Faiji Akbar Liam";
+console.log(name.split(' '));
+
+const friends ="rahim karim jamil mustaq hasib";
+console.log(friends);
+console.log(friends.split(','))
+
+const dept = "tasfiq , musta" ;
+console.log(dept.join("Liam"));
