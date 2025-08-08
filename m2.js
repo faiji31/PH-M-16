860e9dab89168b77525577891a32eacbba3393ce
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-09 00:13:36 +0600

    4th commit

--- m2.js
+++ m2.js
@@ -1,8 +1,16 @@
-const sub = "cse";
-const dept = "Cse";
-if (sub.toUpperCase() === dept.toUpperCase()){
-    console.log("Same Name");
-} 
+// const sub = "cse";
+// const dept = "Cse";
+// if (sub.toUpperCase() === dept.toUpperCase()){
+//     console.log("Same Name");
+// } 
+// else{
+//     console.log("Not Same")
+// }
+const name = " Liam ";
+const fname ="Liam";
+if (name.trim()===fname.trim()){
+    console.log("run");
+}
 else{
-    console.log("Not Same")
+    console.log("not run")
 }
