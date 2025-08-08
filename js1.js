7ce14b91bb3d824fcba461eab896021c89241a7d
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-08 23:57:12 +0600

    2nd commit

---
+++ js1.js
@@ -0,0 +1,9 @@
+const country = "Bangladesh";
+console.log(country);
+const district ='Dhaka';
+console.log(district);
+number = [12, 15 ,16];
+console.log(number.length);
+number [1] =22;
+console.log(number);
+console.log(country.length);
