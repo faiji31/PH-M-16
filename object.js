0472f0809c2b370fac3506eae27bc60e53512731
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-09 22:32:13 +0600

    5th commit

---
+++ object.js
@@ -0,0 +1,16 @@
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+console.log(colors);
+console.log(colors["golden rod"]);
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+ car.passengerCapacity=5;
+ console.log(car);
